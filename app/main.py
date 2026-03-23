d8baf2b32de03b45186338b5ea4e98c144c0c71d
LDukeH <24520378@gm.uit.edu.vb>
2026-03-23 23:33:09 +0700

    fix 2

--- app/main.py
+++ app/main.py
@@ -7,19 +7,12 @@ from app.routes.internal_scraper import router as internal_router
 
 app = FastAPI(title="UIT Student API")
 
-import os
-
-print("MONGO_URL =", os.getenv("MONGO_URL"))
-print("SCRAPER_SECRET =", os.getenv("SCRAPER_SECRET"))
-
 
 @app.get("/")
 def root():
     return {
         "message": "UIT School API is running!",
         "status": "healthy",
-        "MONGO_URL":os.getenv("MONGO_URL"),
-        "SCRAPER_SECRET ": os.getenv("SCRAPER_SECRET")
     }
 
 
